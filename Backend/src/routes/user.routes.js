721a6a6e52f5f96adc5b922671dbe7ef2a31b599
Shivam <shivamch0865@gmail.com>
2026-03-03 12:01:37 +0530

    routes setup

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { registerUser , logoutUser } from "../controllers/user.controller.js";
+import { registerUser , loginUser , logoutUser , getRefreshToken , getRefreshToken } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(registerUser);
+
+router.route("/login").post(loginUser);
+
+router.route("/logout").post(logoutUser);
+
+router.route("/refreshToken").post(getRefreshToken);
 
 export default router;
 
